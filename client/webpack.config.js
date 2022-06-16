750e34cc1646e5a11483478aa43ada9717d4b13f
jinwoo <wwo1278@gmail.com>
2022-06-16 14:06:36 +0900

    setup: client 파일 구조 생성
    Co-authored-by: Park Nojun <Circlewee@users.noreply.github.com>

---
+++ client/webpack.config.js
@@ -0,0 +1,32 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/',
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react'],
+          plugins: ['@babel/plugin-transform-template-literals', '@babel/plugin-transform-arrow-functions'],
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
