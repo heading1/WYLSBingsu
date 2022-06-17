291208df854111b7e567b44160e4af9f08b9aaa3
Circlewee <pnj1118@naver.com>
2022-06-17 22:32:59 +0900

    [#43]feat: 전역 스타일 설정

---
+++ client/src/styles/Theme.tsx
@@ -0,0 +1,15 @@
+interface ITheme {
+  point1: String;
+  point2: String;
+  point3: String;
+  point4: String;
+}
+
+const theme: ITheme = {
+  point1: '#06283D',
+  point2: '#1363DF',
+  point3: '#47B5FF',
+  point4: '#DFF6FF',
+};
+
+export default theme;
