9ec72682c7786fe6d2f8e39fefc22dfd149dc706
jinwoo <wwo1278@gmail.com>
2022-06-17 18:45:13 +0900

    setup: 리액트 전환
    1. React 설치
    2. 웹팩 세팅
    3. 바벨 세팅
    4. react-router-dom 설치
    5. react-dom 설치
    6. typescript 설치
    
    Co-authored-by: Park Nojun <Circlewee@users.noreply.github.com>

---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <>Test</>;
+
+export default App;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,31 +1,42 @@
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const path = require('path');
 
+/*
+mode : 프로덕션 모드인지 개발 모드인지 확인하는 옵션이다.
+devtool : 프로덕션 모드인 경우엔 hidden-source-map을 권장한다. (외부에서 리액트 구조를 확인할 수 없다.)
+resolve : 확장자나 경로를 알아서 처리할 수 있도록 설정하는 옵션이다.
+module : 이 옵션에 설치한 ts-loader와 babel-loader를 설정하면 된다. loader들은 오른쪽에서 왼쪽 방향으로 적용되기 때문에 ts-loader를 babel-loader보다 오른쪽에 위치시켜야 한다.
+output : 번들화 된 파일을 export할 경로와 파일명을 설정한다.
+plugins : 설치한 플러그인을 적용하는 옵션이다
+*/
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.tsx',
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+
   output: {
     path: path.resolve(__dirname, 'dist/'),
+    filename: 'bundle.js',
     publicPath: '/',
   },
+
   plugins: [
-    new HTMLWebpackPlugin({
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+    new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
     }),
   ],
+
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react'],
-          plugins: ['@babel/plugin-transform-template-literals', '@babel/plugin-transform-arrow-functions'],
-        },
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'ts-loader'],
       },
     ],
   },
