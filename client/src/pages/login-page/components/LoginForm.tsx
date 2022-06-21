9bbfb5c4b47771877461cfe925c0866f548d1c01
Circlewee <pnj1118@naver.com>
2022-06-22 01:30:01 +0900

    [#37]feat: LoginForm style 수정

--- client/src/pages/login-page/components/LoginForm.tsx
+++ client/src/pages/login-page/components/LoginForm.tsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form';
+import StyledForm from './LoginFormStyle';
 
 type InputType = {
   email: string;
@@ -11,41 +12,50 @@ const LoginForm: React.FC = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<InputType>();
-  const onSubmit = (data: InputType) => console.log(data);
+
+  const onSubmit = (data: InputType) => {
+    console.log(data);
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>Email</label>
-      <input
-        type="text"
-        {...register('email', {
-          required: true,
-          pattern:
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        })}
-      />
-      {errors.email && errors.email.type === 'required' && (
-        <p>이메일을 입력해주세요.</p>
-      )}
-      {errors.email && errors.email.type === 'pattern' && (
-        <p>올바른 이메일이 아닙니다.</p>
-      )}
-      <label>Password</label>
-      <input
-        type="password"
-        {...register('password', {
-          required: true,
-          validate: (value) => value.length >= 6 && value.length <= 20,
-        })}
-      />
-      {errors.password && errors.password.type === 'requried' && (
-        <p>비밀번호를 입력해주세요.</p>
-      )}
-      {errors.password && errors.password.type === 'validate' && (
-        <p>비밀번호의 길이는 6글자 이상 20글자 이하입니다.</p>
-      )}
+    <StyledForm onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="emailInput">Email</label>
+        <input
+          id="emailInput"
+          type="text"
+          {...register('email', {
+            required: true,
+            pattern:
+              /^(([^<>()[\]\.,;:\s@"]+(\.[^<>()[\]\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          })}
+        />
+        {errors.email && errors.email.type === 'required' && (
+          <p>이메일을 입력해주세요.</p>
+        )}
+        {errors.email && errors.email.type === 'pattern' && (
+          <p>올바른 이메일이 아닙니다.</p>
+        )}
+      </div>
+      <div>
+        <label htmlFor="passwordInput">Password</label>
+        <input
+          id="passwordInput"
+          type="password"
+          {...register('password', {
+            required: true,
+            validate: (value) => value.length >= 6 && value.length <= 20,
+          })}
+        />
+        {errors.password && errors.password.type === 'required' && (
+          <p>비밀번호를 입력해주세요.</p>
+        )}
+        {errors.password && errors.password.type === 'validate' && (
+          <p>비밀번호의 길이는 6글자 이상 20글자 이하입니다.</p>
+        )}
+      </div>
       <input type="submit" value="Login" />
-    </form>
+    </StyledForm>
   );
 };
 
