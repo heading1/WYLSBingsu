5af6b002e14dfb3cf9295993a5e896974684b0d9
Circlewee <pnj1118@naver.com>
2022-06-22 00:51:55 +0900

    [#37]feat: LoginPage style 설정

--- client/src/pages/login-page/LoginPage.tsx
+++ client/src/pages/login-page/LoginPage.tsx
@@ -1,10 +1,11 @@
 import LoginForm from './components/LoginForm';
+import Wrapper from './LoginPageStyle';
 
 const LoginPage: React.FC = () => {
   return (
-    <>
+    <Wrapper>
       <LoginForm />
-    </>
+    </Wrapper>
   );
 };
 
