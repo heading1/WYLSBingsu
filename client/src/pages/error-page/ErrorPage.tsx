89b6ca9707621a3863c3fddae87964382dbffe2d
Circlewee <pnj1118@naver.com>
2022-06-25 15:46:12 +0900

    [#70]feat: ErrorPage 디자인 완성

--- client/src/pages/error-page/ErrorPage.tsx
+++ client/src/pages/error-page/ErrorPage.tsx
@@ -1,5 +1,21 @@
+import { Wrapper, InfoDiv, HomeLink } from './ErrorPageStyle';
+import useDeviceViewport from '@/common/hooks/useDeviceViewport';
+
 const ErrorPage: React.FC = () => {
-  return <div>ErrorPage!</div>;
+  const { deviceWidth, deviceHeight } = useDeviceViewport();
+
+  return (
+    <Wrapper deviceWidth={deviceWidth} deviceHeight={deviceHeight}>
+      <InfoDiv deviceWidth={deviceWidth}>
+        <h1>
+          Bingsu <strong>NOT</strong> FOUND!
+        </h1>
+        <p>빙수가 녹아버렸습니다!</p>
+        <p>다시 빙수로 돌아가주세요.</p>
+        <HomeLink to="/">GO HOME</HomeLink>
+      </InfoDiv>
+    </Wrapper>
+  );
 };
 
 export default ErrorPage;
