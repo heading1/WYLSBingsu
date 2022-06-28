98b1127cffd4adf48a5e83c099f4e5919ee30ff9
Circlewee <pnj1118@naver.com>
2022-06-28 22:32:28 +0900

    [#109]feat: 이메일 검증 폼 코드 이동

--- client/src/pages/register-page/RegisterPage.tsx
+++ client/src/pages/register-page/RegisterPage.tsx
@@ -1,15 +1,10 @@
 import Wrapper from './RegisterPageStyle';
 import RegisterForm from './components/RegisterForm';
-import EmailCertificationForm from './components/EmailCertificationForm';
-import { useState } from 'react';
 
 const RegisterPage: React.FC = () => {
-  const [isSend, setIsSend] = useState<boolean>(false);
-
   return (
     <Wrapper>
-      <RegisterForm emailCertificate={setIsSend} sendingStatus={isSend} />
-      {isSend && <EmailCertificationForm />}
+      <RegisterForm />
     </Wrapper>
   );
 };
--- client/src/pages/register-page/components/RegisterForm.tsx
+++ client/src/pages/register-page/components/RegisterForm.tsx
@@ -2,32 +2,28 @@ import { FormInputType } from '@/types/types';
 import CustomForm from '@/common/form-component/CustomForm';
 import { RegisterButton } from './RegisterFormStyle';
 import { useEffect, useRef } from 'react';
+import useRegister from '../hook/useRegister';
+import EmailCertificationForm from '../components/EmailCertificationForm';
 
-interface RegisterProps {
-  emailCertificate: React.Dispatch<React.SetStateAction<boolean>>;
-  sendingStatus: boolean;
-}
-
-const RegisterForm: React.FC<RegisterProps> = ({
-  emailCertificate,
-  sendingStatus,
-}) => {
+const RegisterForm: React.FC = () => {
   const buttonRef = useRef() as React.MutableRefObject<HTMLButtonElement>;
+  const { isSend, emailValidation } = useRegister();
 
   const onSubmit = (data: FormInputType) => {
-    console.log(data);
-    emailCertificate(true);
+    const addNickname = { ...data, nickName: '건방진 통통이' };
+    emailValidation(addNickname);
   };
 
   useEffect(() => {
-    buttonRef.current.disabled = sendingStatus;
-  }, [sendingStatus]);
+    buttonRef.current.disabled = isSend;
+  }, [isSend]);
 
   return (
     <CustomForm onSubmit={onSubmit}>
-      <RegisterButton type="submit" isDisabled={sendingStatus} ref={buttonRef}>
-        {!sendingStatus ? '회원가입' : '이메일이 전송됐습니다.'}
+      <RegisterButton type="submit" isDisabled={isSend} ref={buttonRef}>
+        {!isSend ? '회원가입' : '이메일이 전송됐습니다.'}
       </RegisterButton>
+      {isSend && <EmailCertificationForm />}
     </CustomForm>
   );
 };
