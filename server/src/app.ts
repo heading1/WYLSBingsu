bf962fe3d76af7543f41c22c5d49bca190f2e770
jaeku <kjklalala2@naver.com>
2022-06-28 14:54:44 +0900

    [#102]feat: error관련 미들웨어를 생성하여 error메세지를 프론트로 전달

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,8 @@
 import cors from 'cors';
 import express from 'express';
-import {
-    userRouter,
-} from './routers';
-  
+import { userRouter } from './routers';
+import { errorLogger, errorHandler } from './middlewares';
+
 const app = express();
 
 // CORS 에러 방지
@@ -15,5 +14,9 @@ app.use(express.json());
 // Content-Type: application/x-www-form-urlencoded 형태의 데이터를 인식하고 핸들링할 수 있게 함.
 app.use(express.urlencoded({ extended: false }));
 app.use('/user', userRouter);
+
+// 미들웨어 (에러를 error.log 파일에 기록 및, 에러를 프론트엔드에 전달)
+app.use(errorLogger);
+app.use(errorHandler);
 
 export { app };
