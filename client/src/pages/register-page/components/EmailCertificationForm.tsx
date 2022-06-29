741aadb43b335d1c5e681611df27cfa07ae86466
Circlewee <pnj1118@naver.com>
2022-06-29 11:51:59 +0900

    [#109]feat: 인증코드 입력 상태 추가

--- client/src/pages/register-page/components/EmailCertificationForm.tsx
+++ client/src/pages/register-page/components/EmailCertificationForm.tsx
@@ -1,9 +1,21 @@
 import { CodeInput, SubmitButton } from './EmailCertificationFormStyle';
+import { useState } from 'react';
 
 const EmailCertificationForm: React.FC = () => {
+  const [authValue, setAuthValue] = useState<string>();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAuthValue(event.target.value);
+  };
+
   return (
     <>
-      <CodeInput type="text" placeholder="인증코드 입력" />
+      <CodeInput
+        type="text"
+        placeholder="인증코드 입력"
+        value={authValue}
+        onChange={handleChange}
+      />
       <SubmitButton>인증</SubmitButton>
     </>
   );
