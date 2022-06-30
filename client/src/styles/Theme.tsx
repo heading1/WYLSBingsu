0f5c4ceea3151f24e5efcbac7b4670c40eb7d133
Circlewee <pnj1118@naver.com>
2022-06-30 22:18:58 +0900

    [#143]style: Theme props에 orange 추가

--- client/src/styles/Theme.tsx
+++ client/src/styles/Theme.tsx
@@ -5,6 +5,7 @@ const theme: DefaultTheme = {
   point2: '#1363DF',
   point3: '#47B5FF',
   point4: '#DFF6FF',
+  orange: '#FF5F00',
   windowWidth: document.documentElement.clientWidth,
   windowHeight: document.documentElement.clientHeight,
 };
