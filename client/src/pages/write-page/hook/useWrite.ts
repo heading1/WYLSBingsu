eacecb856124d175b25c63e8ec7d9715de1aaf0f
Circlewee <pnj1118@naver.com>
2022-07-01 01:10:27 +0900

    [#146]feat: 응답의 결과에 따라 result 처리

--- client/src/pages/write-page/hook/useWrite.ts
+++ client/src/pages/write-page/hook/useWrite.ts
@@ -1,6 +1,10 @@
 import axios, { AxiosError } from 'axios';
 import { useCallback, useState } from 'react';
-import { ArticleInputType, ArticlePostType } from '@/types/interfaces';
+import {
+  ArticleInputType,
+  ArticlePostType,
+  ResponseObjectType,
+} from '@/types/interfaces';
 import { useNavigate } from 'react-router-dom';
 
 interface ArticlePostResponse {
@@ -10,7 +14,10 @@ interface ArticlePostResponse {
 const useWrite = () => {
   const [selectedTopping, setSelectedTopping] = useState<string>('');
   const [isLoading, setLoading] = useState(false);
-  const [result, setResult] = useState({ type: '', content: '' });
+  const [result, setResult] = useState<ResponseObjectType>({
+    type: '',
+    content: '',
+  });
   const [showModal, setShowModal] = useState(false);
   const POST_URI = 'http://localhost:8070/article/register';
   const navigate = useNavigate();
@@ -22,25 +29,35 @@ const useWrite = () => {
         uniqueNumber: '62b949f1071bbbeff46ab87e',
         toppingImage: selectedTopping,
       };
-      console.log('postData', postData);
 
       setLoading(true);
       axios
         .post(POST_URI, postData)
         .then((response) => {
-          // 다시 그사람 빙수로 리턴
+          console.log(response);
+          setResult((previous) => ({
+            ...previous,
+            type: '글 쓰기 성공',
+            content: '글 쓰기에 성공했습니다!',
+          }));
         })
         .catch((err) => {
           if (axios.isAxiosError(err)) {
             const responseError = err as AxiosError<ArticlePostResponse>;
             if (responseError && responseError.response) {
               console.log(responseError.response);
+              setResult((previous) => ({
+                ...previous,
+                type: '글 쓰기 실패',
+                content: 'responseError.response?.data.message',
+              }));
             }
           } else {
             navigate('/error');
           }
         })
         .finally(() => {
+          setShowModal(true);
           setLoading(false);
         });
     },
